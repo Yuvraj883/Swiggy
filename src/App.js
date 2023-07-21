b10617b4a86429a55aabba54eb7857edc060c951
Yuvraj <kingyuvi88@gmail.com>
2023-07-22 03:08:55 +0530

    Imported Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
-const App = ()=>{
+const App = () => {
     return (
         <>
-        <div className="">
-        <h1 className="text-red">Welcome Yuvi!!</h1>
-        </div>
+         <Navbar />
+
         </>
     )
 }

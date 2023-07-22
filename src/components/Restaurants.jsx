0e718a82864bce9cf04d1f51fa4c87389dc80ba2
Yuvraj <kingyuvi88@gmail.com>
2023-07-23 01:59:30 +0530

    Created RestaurantCard

--- src/components/Restaurants.jsx
+++ src/components/Restaurants.jsx
@@ -1,22 +1,37 @@
 import { useState, useEffect } from "react"
+import RestaurantCard from "../RestaurantCard";
 
 const Restaurants= ()=>{
-    const [restraunts, setRestraunts] = useState(null);
+    const [restaurants, setRestaurants] = useState(null);
 
     useEffect(()=>{
-        getRestraunts(); 
+        getRestaurants(); 
     },[])
 
-    async function getRestraunts(){
+    async function getRestaurants(){
         const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"); 
         const json =  await data?.json();
-        console.log(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setRestaurants(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        console.log(restaurants);
+        
+    }
+    if(restaurants === null){
+        return <div>Loading...</div>
     }
 
-
-    return(
+    return (
         <>
-        <h2>Restraunts</h2>
+        <div className="flex flex-row flex-wrap  px-[10%] m-0">
+        {
+            restaurants?.map((restaurant)=>(
+                <RestaurantCard key={restaurant?.info?.id} restaurant={restaurant?.info}/>
+                 
+            )
+            
+            )
+
+        }
+        </div>
         </>
     )
 }

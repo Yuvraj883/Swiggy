ef4a0725e38824ec79449e7d9ecb337576f31018
Yuvraj <kingyuvi88@gmail.com>
2023-07-22 23:33:20 +0530

    Imported Restaurants card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Restaurants from "./components/Restaurants";
 
 const App = () => {
     return (
         <>
          <Navbar />
+         <Restaurants/>
 
         </>
     )

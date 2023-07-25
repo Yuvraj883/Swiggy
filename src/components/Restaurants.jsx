2af032fcfa0bda2a019040205f695285962385fe
Yuvraj <kingyuvi88@gmail.com>
2023-07-26 01:22:45 +0530

    Replaced API calling with the useGetRestaurants hook

--- src/components/Restaurants.jsx
+++ src/components/Restaurants.jsx
@@ -1,25 +1,17 @@
 import { useState, useEffect } from "react"
 import RestaurantCard from "./RestaurantCard";
+import useGetRestaurants from "../utils/useGetRestaurants";
 
 const Restaurants= ()=>{
-    const [restaurants, setRestaurants] = useState(null);
-
-    useEffect(()=>{
-        getRestaurants(); 
-    },[])
-
-    async function getRestaurants(){
-        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"); 
-        const json =  await data?.json();
-        setRestaurants(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-        console.log(restaurants);
-        
-    }
+//   const [restaurants, setRestaurants] = useState(null);
+//   setRestaurants(()=>{useGetRestaurants()});
+const restaurants = useGetRestaurants();
+   
     if(restaurants === null){
         return <div>Loading...</div>
     }
 
-    return (
+    return  (
         <>
         <h1 className="text-2xl font-extrabold  px-[10%] m-2">Restaurants with online food delivery in Delhi</h1>
         <div className="flex flex-row flex-wrap  px-[10%] m-0">

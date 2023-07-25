936aa3b23da2c67ac72f2412a5a19a2ec4a3692e
Yuvraj <kingyuvi88@gmail.com>
2023-07-26 01:43:42 +0530

    Created home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar"
+import Restaurants from "./Restaurants"
+
+const Home = ()=>{
+    return (
+        <>
+        {/* <Navbar/> */}
+        <Restaurants/>
+
+        
+        </>
+    )
+}
+export default Home; 

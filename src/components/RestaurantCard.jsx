78290ce40c8e65fc5b0b88eefc3564326c6db590
Yuvraj <kingyuvi88@gmail.com>
2023-08-01 00:37:51 +0530

    Refactored code:

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -4,17 +4,22 @@ const RestaurantCard = (restaurant) => {
 
     console.log(restro);
     return (<>
-        <div className='h-72 w-[23.5%] mx-2 my-2 cursor-pointer' key={restro?.id}>
+        <div className='h-72 w-[23.5%] mx-2 my-2 cursor-pointer ' key={restro?.id}>
 
-            <img className='h-48 w-full rounded-xl' src={`${IMG_URL}${restro?.cloudinaryImageId}`} alt="restaurant" />
-            <h1 className='line-clamp-1'>{restro?.name} </h1>
+            <img className='h-48 w-full rounded-2xl' src={`${IMG_URL}${restro?.cloudinaryImageId}`} alt="restaurant" />
 
-            <span className='flex flex-row'>
-                <img className='h-6 mx-1' src={require('../assets/green-star.png')} alt="star-icon" />
-                {restro?.avgRating}
-            </span>
+            <div className='px-2'>
+                <h1 className='line-clamp-1 font-bold text-xl'>{restro?.name} </h1>
 
-            <p>{restro?.cuisines?.join(" ")}</p>
+                <span className='flex flex-row text-lg'>
+                    <img className='h-6 mr-1' src={require('../assets/green-star.png')} alt="star-icon" />
+                    {restro?.avgRating}
+                </span>
+
+                <p>{restro?.cuisines?.join(" ")}</p>
+
+            </div>
+
 
         </div>
     </>)

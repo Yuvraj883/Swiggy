3850e9fe5e034bdb7b7b4eba0ff57f0258fdb3ed
Yuvraj <kingyuvi88@gmail.com>
2023-07-31 23:14:15 +0530

    Created TopRestaurants component

--- src/components/TopRestaurants.jsx
+++ src/components/TopRestaurants.jsx
@@ -1,8 +1,17 @@
+import { useContext, useState } from "react";
+import restaurantContext from "../context/restaurantContext";
 
 const TopRestaurants = ()=>{
+    const restaurants = useContext(restaurantContext);
+    const [listOfRestaurants, setListOfRestaurants] = useState(restaurants);
+    console.log(listOfRestaurants);
+
+
     return (
         <>
         <div>
+            <h1>Top restaurants in Delhi</h1>
+        
             
         </div>
         

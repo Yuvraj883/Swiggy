652a2aeaf6147006224ee30aeeb5f037788fa7d7
Yuvraj <kingyuvi88@gmail.com>
2023-08-01 00:25:54 +0530

    Added RestaurantCards display

--- src/components/OnlineRestaurants.jsx
+++ src/components/OnlineRestaurants.jsx
@@ -1,16 +1,27 @@
 import { useContext, useState } from "react";
 import restaurantContext from "../context/restaurantContext";
+import RestaurantCard from "./RestaurantCard";
 
 const OnlineRestaurants = ()=>{
     const restaurants = useContext(restaurantContext);
-    const [listOfRestaurants, setListOfRestaurants] = useState(restaurants);
+    const [listOfRestaurants, setListOfRestaurants] = useState(restaurants?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     console.log(listOfRestaurants);
 
 
     return (
         <>
-        <div>
+        <div >
             <h1>Restaurants with online food delivery in Delhi</h1>
+            <div className="flex flex-row flex-wrap">
+            {
+                listOfRestaurants?.map((restaurant)=>{
+                   return <RestaurantCard restaurant={restaurant}/>
+                
+                })
+            }
+
+            </div>
+            
             
         
             

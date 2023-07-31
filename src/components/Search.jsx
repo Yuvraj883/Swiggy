172f31ea4b4d972f1c2caf113c5b6fef1f86779d
Yuvraj <kingyuvi88@gmail.com>
2023-08-01 02:51:09 +0530

    Implemented Search funcitonality

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,58 @@
+import { useState, useContext, useEffect } from 'react';
+import {BsSearch} from 'react-icons/bs'
+import restaurantContext from '../context/restaurantContext';
+import RestaurantCard from './RestaurantCard';
 const Search = ()=>{
+    const restaurants = useContext(restaurantContext);
+    const [listOfRestaurants, setListOfRestaurants] = useState(null);
+    const [searchText, setSearchText] = useState('');
+    const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+
+
+    useEffect(()=>{
+        setListOfRestaurants(restaurants?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+
+    },[restaurants])
+
+    const handleSearch = () => {
+        console.log("Search Text: ", searchText);
+        const showRestaurants = listOfRestaurants?.filter((restaurant) => {
+          console.log("Restaurant Name: ", restaurant?.info?.name);
+          if (restaurant?.info?.name?.toLowerCase() === searchText?.toLowerCase()) {
+            console.log("Match Found!");
+            return restaurant;
+          }
+          return false;
+        });
+        setFilteredRestaurants(showRestaurants);
+      
+        console.log("showRestaurants: ", filteredRestaurants);
+      };
+      
+      
+
     return (<>
-    <div>
-        <input type="text" placeholder="Search restaurants"/>
+    <div className='flex flex-row justify-center'>
+
+    
+    <div className="my-8 w-[60%] flex justify-between items-center pr-2 rounded-full border-2 border-black ">
+        <input
+        value={searchText}
+        onChange={(e)=>{
+            setSearchText(e.target.value);
+        }}
+        className=' p-3 mr-2 w-[95%] focus:border-orange-500 rounded-l-full' type="text" placeholder="Search restaurants"/>
+        <BsSearch onClick={handleSearch} className='cursor-pointer text-[#3D4152] hover:text-orange-500' size={30}/>
+    </div>
+
+    </div>
+    <div className='flex justify-start'>
+        {
+            filteredRestaurants?.map((restaurant)=>{
+                return <RestaurantCard restaurant={restaurant}/>
+            })
+
+        }
     </div>
     </>)
 }

2bdc82d2bbe0085552071a8067b82ba163eb01bf
Yuvraj <kingyuvi88@gmail.com>
2023-07-31 23:12:32 +0530

    Imported useGetRestaurants and passed it as value in provider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
-
+import { useEffect, useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import RestaurantContext from './context/restaurantContext';
+// import useGetRestaurants from './utils/useGetRestaurants';
+import TopRestaurants from './components/TopRestaurants';
 
 function App() {
+  const [restaurants, setRestaurants] = useState(null);
+
+  useEffect(() => {
+
+    fetchRestaurants();
+
+  }, []);
+
+  useEffect(()=>{
+    console.log(restaurants);
+
+  },[restaurants]);
+
+
+  const fetchRestaurants = async () => {
+    const url = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+    const response = await fetch(url);
+    response.json().then((data) => {
+      console.log(data);
+      setRestaurants(data);
+    })
+
+  }
+
+  if(!restaurants){
+   return <div>...Loading</div>
+  }
+
+
   return (
-   <>
-   <div classname="px-[10%]">
-   <Navbar/>
-   </div>
-   </>
+    <>
+      <RestaurantContext.Provider value={restaurants}>
+
+        <div className="px-[10%]">
+          <Navbar />
+          <TopRestaurants />
+        </div>
+      </RestaurantContext.Provider>
+
+    </>
   );
 }
 

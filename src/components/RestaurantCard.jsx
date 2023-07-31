10f248e15141c728e54d57c6efddf768f9c23d6f
Yuvraj <kingyuvi88@gmail.com>
2023-08-01 04:12:59 +0530

    Refactored code

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -1,12 +1,14 @@
-import { IMG_URL } from '../utils/helper'
+import { IMG_CDN_URL } from '../utils/helper'
+import {Link } from 'react-router-dom'
 const RestaurantCard = (restaurant) => {
     const restro = restaurant?.restaurant?.info;
 
     console.log(restro);
     return (<>
+    <Link to={`restaurant/${restro?.id}`}>
         <div className='h-72 w-[23.5%] mx-2 my-2 cursor-pointer ' >
 
-            <img className='h-48 w-full rounded-2xl' src={`${IMG_URL}${restro?.cloudinaryImageId}`} alt="restaurant" />
+            <img className='h-48 w-full rounded-2xl' src={`${IMG_CDN_URL}${restro?.cloudinaryImageId}`} alt="restaurant" />
 
             <div className='px-2'>
                 <h1 className='line-clamp-1 font-bold text-xl'>{restro?.name} </h1>
@@ -22,6 +24,7 @@ const RestaurantCard = (restaurant) => {
 
 
         </div>
+        </Link>
     </>)
 }
 

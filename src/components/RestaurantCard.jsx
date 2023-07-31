ea8f1831dd9d9b2f16e89a358f7d5d499de2f21c
Yuvraj <kingyuvi88@gmail.com>
2023-08-01 03:58:24 +0530

    Removed key

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -4,7 +4,7 @@ const RestaurantCard = (restaurant) => {
 
     console.log(restro);
     return (<>
-        <div className='h-72 w-[23.5%] mx-2 my-2 cursor-pointer ' key={restro?.id}>
+        <div className='h-72 w-[23.5%] mx-2 my-2 cursor-pointer ' >
 
             <img className='h-48 w-full rounded-2xl' src={`${IMG_URL}${restro?.cloudinaryImageId}`} alt="restaurant" />
 

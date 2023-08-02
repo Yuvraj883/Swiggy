0397156e1552f91debd2510f66ca1b479cf34e0e
Yuvraj <kingyuvi88@gmail.com>
2023-08-02 23:53:09 +0530

    Added error element

--- src/router.js
+++ src/router.js
@@ -3,11 +3,13 @@ import App from './App';
 import Home from './components/Home';
 import Search from './components/Search';
 import RestaurantPage from './components/RestaurantPage';
+import ErrorPage from './components/ErrorPage';
 
 const router = createBrowserRouter([
     {
         path:'/',
         element: <App/>,
+        errorElement:<ErrorPage/>,
         children:[
             {
                 path:'/',

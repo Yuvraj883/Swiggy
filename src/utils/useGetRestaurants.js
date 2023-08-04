567d26124718bdf62b73b27748a5944e5a3ff9aa
Yuvraj <kingyuvi88@gmail.com>
2023-08-04 23:22:44 +0530

    Refactored code

--- src/utils/useGetRestaurants.js
+++ src/utils/useGetRestaurants.js
@@ -1,14 +1,6 @@
-import { useEffect, useState } from "react";
+
 
 const useGetRestaurants = ()=>{
-    const [restaurants, setRestaurants] = useState(null);
-  
-  useEffect(()=>{
-    const res = fetchRestaurants();
-   setRestaurants(res);
-//    console.log(restaurants);
-        
-    },[]);
   
      const fetchRestaurants = async ()=>{
         const url = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
@@ -19,7 +11,7 @@ const useGetRestaurants = ()=>{
       
     }
 
-    return restaurants;
+    return {fetchRestaurants};
 
 }
 export default useGetRestaurants;

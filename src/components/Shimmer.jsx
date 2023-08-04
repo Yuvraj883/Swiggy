74e67f20367158ace3a15779e9517685f721d0e9
Yuvraj <kingyuvi88@gmail.com>
2023-08-04 23:24:09 +0530

    Added key to the re-rendering div

--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -4,8 +4,8 @@ const Shimmer = ()=>{
         <div className="flex flex-wrap justify-start px-[10%]">
 
             {
-              Array(10).fill("").map((a)=>{
-                return (<div className="h-56 w-[23%] m-2 bg-gray-300">
+              Array(10).fill("").map((a, index)=>{
+                return (<div className="h-56 w-[23%] m-2 bg-gray-300" key={index}>
 
                 </div>)
                })

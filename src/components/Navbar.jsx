3b923b7d37d8610d2d7ab37f66a979701444f4a0
Yuvraj <kingyuvi88@gmail.com>
2023-08-06 20:47:10 +0530

    Refactored Code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,28 @@
-import {  Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const Navbar = ()=>{
-    return(
+const Navbar = () => {
+    return (
         <>
-        <nav className="px-[10%] flex justify-between items-center p-2 shadow-md ">
-            <Link to="/">
-            <div className="flex flex-row items-center">
-            
-                <img src={require("../assets/swiggy-logo.png")} 
-                className="h-16 w-auto"
-                alt="Swiggy logo"/>
-                <h1 className="text-orange-400 font-bold text-4xl ">Swiggy</h1>
-            </div>
-            </Link>
+            <nav className="px-[10%] flex justify-between items-center p-2 shadow-md ">
+                <Link to="/">
+                    <div className="flex flex-row items-center">
 
-            <ul className="text-lg font-semibold flex text-[#3D4152]">
+                        <img src={require("../assets/swiggy-logo.png")}
+                            className="h-16 w-auto"
+                            alt="Swiggy logo" />
+                        <h1 className="text-orange-400 font-bold text-4xl ">Swiggy</h1>
+                    </div>
+                </Link>
 
-                <li className="hover:text-orange-400 p-2 cursor-pointer"><Link activeClassName="text-orange-500" to="/search">Search</Link></li>
-                <li className="hover:text-orange-400 p-2 cursor-pointer">Offers</li>
-                <li className="hover:text-orange-400 p-2 cursor-pointer">Help</li>
-                <li className="hover:text-orange-400 p-2 cursor-pointer">Sign in</li>
-                <li className="hover:text-orange-400 p-2 cursor-pointer">Cart</li>
-            </ul>
-        </nav>
+                <ul className="text-lg font-semibold flex text-[#3D4152]">
+
+                    <Link> <li className="hover:text-orange-400 p-2 cursor-pointer">Search</li></Link>
+                    <Link> <li className="hover:text-orange-400 p-2 cursor-pointer">Offers</li></Link>
+                    <Link>  <li className="hover:text-orange-400 p-2 cursor-pointer">Help</li></Link>
+                    <Link> <li className="hover:text-orange-400 p-2 cursor-pointer">Sign in</li></Link>
+                    <Link> <li className="hover:text-orange-400 p-2 cursor-pointer">Cart</li></Link>
+                </ul>
+            </nav>
         </>
     )
 }
